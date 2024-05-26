0c63172f112e3b9faafa3f47e65ebcfc5fe04c31
rory <rory@brainiumstudios.com>
2024-05-26 10:35:44 -0700

    basic prompt, run command

---
+++ jlox/Scanner.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    public Scanner(String source) {
+    }
+
+    public List<Token> scanTokens() {
+        return new ArrayList<>();
+    }
+}
