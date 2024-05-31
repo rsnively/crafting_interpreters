fcceed0d346d99225f14ecceacd1eb1cb89d4b88
rory <rory@brainiumstudios.com>
2024-05-31 12:13:17 -0700

    start of clox

---
+++ clox/build/src/CMakeFiles/clox.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/rory/Documents/projects/crafting_interpreters/clox/src/chunk.c" "/Users/rory/Documents/projects/crafting_interpreters/clox/build/src/CMakeFiles/clox.dir/chunk.c.o"
+  "/Users/rory/Documents/projects/crafting_interpreters/clox/src/main.c" "/Users/rory/Documents/projects/crafting_interpreters/clox/build/src/CMakeFiles/clox.dir/main.c.o"
+  "/Users/rory/Documents/projects/crafting_interpreters/clox/src/memory.c" "/Users/rory/Documents/projects/crafting_interpreters/clox/build/src/CMakeFiles/clox.dir/memory.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ clox/src/main.c
@@ -0,0 +1,14 @@
+#include "common.h"
+#include "chunk.h"
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
+    return 0;
+}
