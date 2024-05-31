509c60f23255d04076d96e7a48624de9de6f008b
rory <rory@brainiumstudios.com>
2024-05-31 12:23:50 -0700

    disassembler

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 #include <stdio.h>
 
@@ -8,6 +9,8 @@ int main(int argc, const char *argv[])
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 
     return 0;
