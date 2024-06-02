2be75a658368a34b4ff23ae8c4bcf73dfa035b5c
rory <rory@brainiumstudios.com>
2024-06-02 10:43:43 -0700

    vm setup

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,11 +1,13 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 #include <stdio.h>
 
 int main(int argc, const char *argv[])
 {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
@@ -16,7 +18,9 @@ int main(int argc, const char *argv[])
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
 
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
