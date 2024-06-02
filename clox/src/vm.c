97f8013d43c7b3965b0ff6494811bdd58728ef57
rory <rory@brainiumstudios.com>
2024-06-02 11:06:21 -0700

    negation

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -60,6 +60,11 @@ static InterpretResult run()
             push(constant);
             break;
         }
+        case OP_NEGATE:
+        {
+            push(-pop());
+            break;
+        }
         case OP_RETURN:
         {
             printValue(pop());
