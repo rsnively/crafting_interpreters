7a6b0a838ce08756befd93d9a45df2ced24ce747
rory <rory@brainiumstudios.com>
2024-06-19 16:38:39 -0700

    functions

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -147,7 +147,7 @@ static TokenType identifierType()
             case 'o':
                 return checkKeyword(2, 1, "r", TOKEN_FOR);
             case 'u':
-                return checkKeyword(2, 1, "u", TOKEN_FUN);
+                return checkKeyword(2, 1, "n", TOKEN_FUN);
             }
         }
         break;
