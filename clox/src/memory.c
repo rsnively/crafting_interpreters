01d529f42a1c2ec03a2fc72dddae631cdc60ba41
rory <rory@brainiumstudios.com>
2024-06-28 11:40:54 -0700

    declaring classes

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -80,6 +80,11 @@ static void blackenObject(Obj *object)
     printf("\n");
 #endif
     switch (object->type) {
+        case OBJ_CLASS: {
+            ObjClass *klass = (ObjClass*)object;
+            markObject((Obj*)klass->name);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure*) object;
             markObject((Obj*)closure->function);
@@ -112,6 +117,11 @@ static void freeObject(Obj *object)
 
     switch (object->type)
     {
+    case OBJ_CLASS:
+    {
+        FREE(ObjClass, object);
+        break;
+    }
     case OBJ_CLOSURE:
     {
         ObjClosure *closure = (ObjClosure *)object;
