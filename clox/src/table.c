d9b11412939be0635ab8a5375419d581c40fa0ad
rory <rory@brainiumstudios.com>
2024-06-30 12:58:24 -0700

    optimize % in hash tables

--- clox/src/table.c
+++ clox/src/table.c
@@ -23,7 +23,7 @@ void freeTable(Table *table)
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
 {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry *tombstone = NULL;
     for (;;)
     {
@@ -46,7 +46,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
         {
             return entry;
         }
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -137,7 +137,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
     if (table->count == 0)
         return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;)
     {
         Entry *entry = &table->entries[index];
@@ -152,7 +152,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
         {
             return entry->key;
         }
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
